9ad7ae415f3b43b7a0717b87503219fce5835d7b
Victor Olsson <victor.olsson1997@outlook.com>
2021-03-22 13:50:17 +0100

    Removed repetitive code

--- client/src/components/BowlingSchema.vue
+++ client/src/components/BowlingSchema.vue
@@ -48,31 +48,31 @@ export default {
       historySize: 0,
       updSchemaObj: {},
       totalTries: 0,
-      currEntry: {},
+      currEntry: {}
     };
   },
-  created: function () {
+  created: function() {
     for (let i = 0; i < 9; i++) {
       this.schema[i] = {
         first: "0",
         second: "0",
-        total: "0",
+        total: "0"
       };
       this.schema[9] = {
         first: "0",
         second: "0",
         bonus: "0",
-        total: "0",
+        total: "0"
       };
     }
   },
   props: ["updateTrigger"],
   watch: {
-    updateTrigger: function () {
+    updateTrigger: function() {
       /*This function makes changes to an array- and for vue to react to 
       these changes, the array is changed using the wrapped array functions (splice in this case).*/
       this.assignNewEntry();
-    },
+    }
   },
   methods: {
     assignNewEntry() {
@@ -125,42 +125,34 @@ export default {
       var newSchemaObj = this.schema[this.schemaIndex];
       if (this.turn == 1) {
         newSchemaObj.first = "X";
-        this.schema.splice(this.schemaIndex, 1, newSchemaObj);
         this.bonusMax = 2;
-        this.bonusCounter++;
         this.extended = true;
       } else if (this.turn == 2) {
         newSchemaObj.second = "X";
-        this.schema.splice(this.schemaIndex, 1, newSchemaObj);
-        this.bonusCounter++;
       } else if (this.turn == 3) {
         newSchemaObj.bonus = "X";
-        this.schema.splice(this.schemaIndex, 1, newSchemaObj);
-        this.bonusCounter++;
       }
+      this.schema.splice(this.schemaIndex, 1, newSchemaObj);
+      this.bonusCounter++;
     },
     handleSpareBonus() {
       var newSchemaObj = this.schema[this.schemaIndex];
       if (this.turn == 2) {
         newSchemaObj.second = "/";
-        this.schema.splice(this.schemaIndex, 1, newSchemaObj);
         this.bonusMax = 1;
-        this.bonusCounter++;
         this.extended = true;
       } else if (this.turn == 3) {
         newSchemaObj.bonus = "/";
-        this.schema.splice(this.schemaIndex, 1, newSchemaObj);
-        this.bonusCounter++;
       }
+      this.schema.splice(this.schemaIndex, 1, newSchemaObj);
+      this.bonusCounter++;
     },
     handleRegularBonus(entry) {
       var newSchemaObj = this.schema[this.schemaIndex];
       if (this.turn == 1) {
         newSchemaObj.first = entry.pinsHit;
-        this.schema.splice(this.schemaIndex, 1, newSchemaObj);
       } else if (this.turn == 2) {
         newSchemaObj.second = entry.pinsHit;
-        this.schema.splice(this.schemaIndex, 1, newSchemaObj);
         if (!this.extended) {
           this.bonusMax = -1;
           this.noBonusOnFinal = true;
@@ -168,9 +160,9 @@ export default {
       } else {
         /*For last bonus ball if not spare or strike*/
         newSchemaObj.bonus = entry.pinsHit;
-        this.schema.splice(this.schemaIndex, 1, newSchemaObj);
         this.bonusCounter++;
       }
+      this.schema.splice(this.schemaIndex, 1, newSchemaObj);
     },
     updSchemaTotal() {
       /*Get the raw total score*/
@@ -211,10 +203,12 @@ export default {
     },
     regularEval() {
       if (
-        !this.currEntry.spare &&
-        !this.currEntry.strike &&
-        this.turn == 2 &&
-        !this.lastField || this.noBonusOnFinal /*<--- In case we have failed to get a bonus, we want to show the final score*/
+        (!this.currEntry.spare &&
+          !this.currEntry.strike &&
+          this.turn == 2 &&
+          !this.lastField) ||
+        this.noBonusOnFinal /*<--- In case we have failed to get a bonus, we want to show 
+        the final score on last fields turn 2*/
       ) {
         this.updSchemaObj = this.schema[this.schemaIndex];
         this.updSchemaObj.total = this.totalContainer;
@@ -305,8 +299,8 @@ export default {
         this.turn = 1;
         this.schemaIndex++;
       }
-    },
-  },
+    }
+  }
 };
 </script>
 
